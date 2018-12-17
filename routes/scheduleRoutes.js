094fb37187b146eaf87dec9cb762f13118fbe0be
Surenkhuu Narankhuu <snarankhuu@mum.edu>
2018-12-16 22:02:55 -0600

    mongodb config

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -4,8 +4,27 @@ const Schedule = mongoose.model("schedules");
 
 module.exports = app => {
 
-    app.post('/schedule', (req, res) => {
-        res.end()
+    app.get('/schedule', async (req, res) => {
+        const newSchedule = new Schedule({
+            avalaibleSeat: 12,
+            from: "fairfield",
+            to: "chicago",
+        })
+        await newSchedule.save((err, data)=>{
+            if(err) console.log(err)
+            console.log(data)
+            res.json(newSchedule)
+        })
+ 
+        
+    });
+    app.get('/schedule/list', (req, res) => {
+        Schedule.find({}, (err, schedules)=>{
+            if(err) console.log(err)
+            console.log(schedules)
+            res.json(schedules)
+        })
+        
     });
 
 };
