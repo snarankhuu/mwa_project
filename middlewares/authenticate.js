52d335873ad22ca7ee31bf6973b11417bf93744f
Daniel <kyle.sontrinh@gmail.com>
2018-12-17 22:35:13 -0600

    token get user

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,8 @@
-const expressJwt = require('express-jwt');
 const fs = require('fs');
 const RSA_PUBLIC_KEY = fs.readFileSync('./public.key');
 const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
 module.exports = (req, res, next) => {
     if (req.url === '/'
@@ -27,11 +28,16 @@ module.exports = (req, res, next) => {
                     if (err)
                         return res.status(500).send({auth: false, message: 'Token authentication failed.'});
                     else {
-                        req._id = decoded._id;
+                        let email = decoded.sub ;
+                        User.findOne({email: email}, (err, user) => {
+                            if (err) console.log(err);
+                            req.user = user;
+                            next();
+                        });
                     }
                 }
             )
         }
     }
-    next();
+    // next();
 };
